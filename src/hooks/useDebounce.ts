b2d029fde8aac49599d7f3aa09525cd7c1231d19
Kreoniz <kreoniz@mail.ru>
2025-07-03 11:20:23 +0600

    feat: add search

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,50 @@
+import { useEffect, useRef, useCallback } from 'react';
+
+type AnyFunction = (...args: any[]) => any;
+
+/*
+export function useDebounce<T extends AnyFunction>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const callbackRef = useRef(func);
+
+  useEffect(() => {
+    callbackRef.current = func;
+  }, [func]);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  return useCallback(
+    (...args: Parameters<T>) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+
+      timeoutRef.current = setTimeout(() => {
+        callbackRef.current(...args);
+      }, delay);
+    },
+    [delay]
+  );
+}
+*/
+
+export function useDebounce(callback: AnyFunction, delay: number) {
+  let timeoutId: NodeJS.Timeout;
+
+  return (...args) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      callback.apply(this, args);
+    }, delay);
+  };
+}
--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,13 +1,33 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapWidget } from '@/features/map';
 import styles from './MapPage.module.scss';
 import MapIcon from '@/assets/icons/map.svg?react';
 import LoadFileIcon from '@/assets/icons/load-file.svg?react';
+import SearchIcon from '@/assets/icons/search.svg?react';
 import Papa from 'papaparse';
 import type { MapMarker } from '@/types';
+import { useDebounce } from '@/hooks';
 
 export function MapPage() {
   const [markers, setMarkers] = useState<MapMarker[]>([]);
+  const [searchedMarkers, setSearchedMarkers] = useState<MapMarker[]>([]);
+
+  const search = (query: string) => {
+    const processedQuery = query.trim().toLowerCase();
+
+    setSearchedMarkers(
+      markers.filter((marker) => {
+        const processedMarker = marker.name.trim().toLowerCase();
+        return processedMarker.includes(processedQuery);
+      })
+    );
+  };
+
+  const debouncedSearch = useDebounce(search, 300);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedSearch(e.currentTarget.value);
+  };
 
   useEffect(() => {
     const loadData = async () => {
@@ -16,6 +36,7 @@ export function MapPage() {
 
       const parsed = Papa.parse(data, { header: true, skipEmptyLines: true });
       setMarkers(parsed.data as MapMarker[]);
+      setSearchedMarkers(parsed.data as MapMarker[]);
     };
 
     loadData();
@@ -36,9 +57,17 @@ export function MapPage() {
           </div>
 
           <div className={styles.sidebarContent}>
-            <div className={styles.search}>Поиск</div>
+            <label className={styles.search} htmlFor="searchInput">
+              <input
+                id="searchInput"
+                placeholder="Поиск..."
+                onChange={handleSearch}
+                type="search"
+              />
+              <SearchIcon />
+            </label>
 
-            {markers.map((marker, i) => (
+            {searchedMarkers.map((marker, i) => (
               <div key={i} className={styles.accordion}>
                 {marker.name}
               </div>
@@ -48,7 +77,7 @@ export function MapPage() {
       </div>
 
       <main className={styles.main}>
-        <MapWidget markers={markers} />
+        <MapWidget markers={searchedMarkers} />
       </main>
     </div>
   );
